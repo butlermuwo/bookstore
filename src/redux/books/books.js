c2a67a3d6bd463d8ec5dc05b12b07851788bc60b
unknown <butlermuwo@gmail.com>
2022-03-04 12:28:23 +0200

    Update book.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,49 +1,29 @@
-import book from "../../components/Book";
+const ADD_BOOK = 'BookStores/books/ADD_BOOK';
+const REMOVE_BOOK = 'BookStores/books/REMOVE_BOOK';
 
-const ADD_BOOK = 'bookStore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const GET_BOOK = 'bookstore/books/GET_BOOK';
+const initialState = [];
 
-const handleData = (data) => {
-    const books = [];
-    const keys = Object.keys(data);
-
-    keys.forEach((key, index) => {
-        const book = data[keys[index]];
-        book[0].item_id = key;
-    });
-    return book;
-};
-
-// Actions creators
-
-export const addBook = (book) => async (dispatch) => {
-    type: ADD_BOOK,
-    payload
-}
-export const removeBook = (bookId) => async (dispatch) =>{
-    try {
-        await AudioParam.deleteBook(bookId);
-
-        dispatch({type: REMOVE_BOOK,payload: bookId});
-    } catch (error) {
-        throw new Error(error.message);
-    }
-
-};
+// ACTION CREATORS
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+export const removeBook = (bookId) => ({
+  type: REMOVE_BOOK,
+  id: bookId,
+});
 
 // REDUCER
-const reducer =  (state = [], action) => {
-switch (action.type) {
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case ADD_BOOK:
-       return [...state, action.payload]
-
+      return [...state, action.payload];
     case REMOVE_BOOK:
-return state.filter((book) => book.item_id !== action.payload);
-
+      return state.filter((book) => book.id !== action.id);
     default:
-        return state;
-    }
+      return state;
+  }
 };
 
 export default reducer;
